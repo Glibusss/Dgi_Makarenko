e2802022b959550d4a36150f53bd0626561278ce
Glibusss <expansifff@gmail.com>
2024-04-20 17:55:21 +0300

    split to bytearray

--- file_parser/wav_class.py
+++ file_parser/wav_class.py
@@ -1,22 +1,24 @@
 import os
+import numpy as np
+
 class WavFile:
 
     __header_values = {}
 
     def make_header(self):
-        self.__header_values['riff_chunk_id'] = self.__header[0:4].decode("utf-8")
-        self.__header_values['riff_chunk_size'] = int.from_bytes(self.__header[4:8], byteorder="little")
-        self.__header_values['wave_format'] = self.__header[8:12].decode("utf-8")
-        self.__header_values['fmt_chunk_id'] = self.__header[12:16].decode("utf-8")
-        self.__header_values['fmt_chunk_size'] = int.from_bytes(self.__header[16:20], byteorder="little")
-        self.__header_values['audio_format'] = int.from_bytes(self.__header[20:22], byteorder="little")
-        self.__header_values['num_channels'] = int.from_bytes(self.__header[22:24], byteorder="little")
-        self.__header_values['sample_rate'] = int.from_bytes(self.__header[24:28], byteorder="little")
-        self.__header_values['byte_rate'] = int.from_bytes(self.__header[28:32], byteorder="little")
-        self.__header_values['block_align'] = int.from_bytes(self.__header[32:34], byteorder="little")
-        self.__header_values['bits_per_sample'] = int.from_bytes(self.__header[34:36], byteorder="little")
-        self.__header_values['data_chunk_id'] = self.__header[36:40].decode("utf-8")
-        self.__header_values['data_chunk_size'] = int.from_bytes(self.__header[40:44], byteorder="little")
+        self.__header_values['riff_chunk_id'] = self.__file_data[0:4].decode("utf-8")
+        self.__header_values['riff_chunk_size'] = int.from_bytes(self.__file_data[4:8], byteorder="little")
+        self.__header_values['wave_format'] = self.__file_data[8:12].decode("utf-8")
+        self.__header_values['fmt_chunk_id'] = self.__file_data[12:16].decode("utf-8")
+        self.__header_values['fmt_chunk_size'] = int.from_bytes(self.__file_data[16:20], byteorder="little")
+        self.__header_values['audio_format'] = int.from_bytes(self.__file_data[20:22], byteorder="little")
+        self.__header_values['num_channels'] = int.from_bytes(self.__file_data[22:24], byteorder="little")
+        self.__header_values['sample_rate'] = int.from_bytes(self.__file_data[24:28], byteorder="little")
+        self.__header_values['byte_rate'] = int.from_bytes(self.__file_data[28:32], byteorder="little")
+        self.__header_values['block_align'] = int.from_bytes(self.__file_data[32:34], byteorder="little")
+        self.__header_values['bits_per_sample'] = int.from_bytes(self.__file_data[34:36], byteorder="little")
+        self.__header_values['data_chunk_id'] = self.__file_data[36:40].decode("utf-8")
+        self.__header_values['data_chunk_size'] = int.from_bytes(self.__file_data[40:44], byteorder="little")
 
     def __init__(self, file_path):
         dirname = os.path.dirname(__file__)
@@ -24,12 +26,9 @@ class WavFile:
 
         try:
             with open(filename, "rb") as file:
-                file_data = file.read()
+                self.__file_data = file.read()
         except FileNotFoundError:
             return None
-        
-        self.__header = file_data[:44]
-        self.__payload = file_data[44:]
 
         self.make_header()
 
@@ -44,3 +43,17 @@ class WavFile:
             result = None
 
         return result
+
+    def split_to_chunks(self, max_time):
+        
+        sample_rate = self.get_header_value('sample_rate')
+
+        if not sample_rate:
+            return []
+        
+        chunk_size = int(max_time * sample_rate)
+        bytes_array = bytearray(self.__file_data)
+        chunks = [bytes_array[i:i+chunk_size] for i in range(0, len(bytes_array), chunk_size)]
+
+        return chunks
+        
